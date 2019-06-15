19d7f524e004490b028b45c2baf1cb42c5381273
satveerbhantoo <satveerbhantoo011@gmail.com>
2019-06-15 20:27:58 +0400

    production precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.assets.precompile += ['active_admin.css', 'active_admin/print.css', 'active_admin.js']
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
