863f14d39bd4554162f446ed9b4685797c9b42d8
aviramah0307 <avishek.ramah@sc-mauritius.com>
2019-06-16 02:26:28 +0400

    fix

--- app/assets/javascripts/home/home.controller.js
+++ app/assets/javascripts/home/home.controller.js
@@ -2,7 +2,7 @@
 
   'use strict';
 
-  function HomeController($scope, Auth) {
+  function HomeController($scope, Auth,$interval) {
     var vm = this;
 
     // callable methods on the vm
@@ -26,6 +26,7 @@
         return vm.user = user;
     };
 
+
     //event listeners for user authentication and logout
     $scope.$on('devise:new-registration', function(e, user){
         return vm.user = user;
@@ -36,6 +37,24 @@
     $scope.$on('devise:logout', function(e, user){
         return vm.user = {};
     });
+
+    //controller method
+ $interval(function(){
+    var endDate = new Date(2044, 4, 5, 0, 0, 0);
+    var todayDate = new Date();
+    todayDate.setFullYear(2024);
+    $scope.$evalAsync(function(){
+        var diff = new Date(endDate.getTime() - todayDate.getTime());
+        $scope.day = (diff.getUTCFullYear()- 1970)*365;
+        $scope.dayP = (Math.round((diff.getUTCFullYear()- 1970)*365)/73) > 50 ? 'over50 p'+(Math.round((diff.getUTCFullYear()- 1970)*365)/73) : 'p'+(Math.round((diff.getUTCFullYear()- 1970)*365)/73);
+        $scope.hour = diff.getUTCHours();
+        $scope.hourP = Math.round(diff.getUTCHours()/0.24) > 50 ? 'over50 p'+Math.round(diff.getUTCHours()/0.24) : 'p'+Math.round(diff.getUTCHours()/0.24) ;
+        $scope.minute = diff.getUTCMinutes();
+        $scope.minuteP = Math.round(diff.getUTCMinutes()/0.60) > 50 ? 'over50 p'+Math.round(diff.getUTCMinutes()/0.60) : 'p'+Math.round(diff.getUTCMinutes()/0.60);
+        $scope.second = diff.getUTCSeconds();
+        $scope.secondP = Math.round(diff.getUTCSeconds()/0.60) > 50 ? 'over 50 p'+Math.round(diff.getUTCSeconds()/0.60) : 'p'+Math.round(diff.getUTCSeconds()/0.60);
+    })
+}, 1000);
 
   };
 
