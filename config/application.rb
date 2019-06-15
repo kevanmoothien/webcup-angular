a3126a4ecf33201a0d70f0e6c1fa5cb9a228d2f5
satveerbhantoo <satveerbhantoo011@gmail.com>
2019-06-15 19:35:50 +0400

    fuckthisshit

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,12 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+if defined?(Bundler)
+  # If you precompile assets before deploying to production, use this line
+  # Bundler.require(*Rails.groups(:assets => %w(development test)))
+  # If you want your assets lazily compiled in production, use this line
+  Bundler.require(:default, :assets, Rails.env)
+end
 
 module Webcup
   class Application < Rails::Application
@@ -15,5 +20,16 @@ module Webcup
    config.to_prepare do
      DeviseController.respond_to :html, :json
    end
+
+   # Enable the asset pipeline
+   config.assets.enabled = true
+
+   # Disable application initialize on precompile (heroku faq: fixes database access error on precompile)
+   config.assets.initialize_on_precompile = false
+
+   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
+   config.assets.precompile += %w( active_admin.css active_admin/print.css active_admin.js )
+
+   
   end
 end
